644b8fca8bb160dbe31bc494359faa2634dee5e8
Bogdan Movileanu <movileanu_bogdan@yahoo.com>
2021-10-04 19:33:32 +0300

    clean up template

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://www.redpack.org",
     title: "redpack-web",
   },
   plugins: ["gatsby-plugin-gatsby-cloud"],
