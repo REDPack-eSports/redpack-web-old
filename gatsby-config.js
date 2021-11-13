658aec2d6e76db5b2295421392fe76e640bae8df
Bogdan Movileanu <movileanu_bogdan@yahoo.com>
2021-11-13 13:41:13 +0200

    added minimal required plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,35 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.redpack.org",
-    title: "redpack-web",
+    siteUrl: 'https://www.redpack.org',
+    title: 'REDPack',
   },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
+  plugins: [
+    'gatsby-plugin-gatsby-cloud',
+    {
+      resolve: 'gatsby-source-notion',
+      options: {
+        databases: [
+          '2bc9868a856d4ca6a166436f04979d16',
+          '647ac87f7e4c4a6187e78f4e2a8faf50',
+          'fa9789f64cb84bd48f74d65b13c720dd'
+        ]
+      }
+    },
+    'gatsby-plugin-sass',
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 750,
+              linkImagesToOriginal: false
+            }
+          }
+        ]
+      }
+    }
+  ],
 };
