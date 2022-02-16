3adba70402cf86b27d8e14d2215c335482823ae6
Movileanu, Bogdan (PERO-SF) <bogdan.movileanu@porsche-engineering.ro>
2022-02-16 18:33:13 +0200

    fixed social media button bug

--- src/components/social_media_bar.jsx
+++ src/components/social_media_bar.jsx
@@ -10,8 +10,8 @@ const SocialMediaBar = () => {
             <h2 className="text-white">ON SOCIAL MEDIA</h2>
         </div>
         <div className="flex my-auto text-7xl text-cyan-400 ml-24 gap-16"> 
-            <BsYoutube className="scale-125"/>
-            <BsTwitter className="scale-125"/>
+            <BsYoutube />
+            <BsTwitter />
             <BsInstagram />
             <BsFacebook />
             <BsTwitch />
