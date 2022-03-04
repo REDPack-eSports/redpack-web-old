0e08de56da7ab4b797570a6fa7e828500b68ff8b
Movileanu, Bogdan (PERO-SF) <bogdan.movileanu@porsche-engineering.ro>
2022-03-04 16:43:07 +0200

    unlinked creator and team data from their components

--- src/components/TeamsShowcase.jsx
+++ src/components/TeamsShowcase.jsx
@@ -3,63 +3,9 @@ import React from 'react'
 import SectionHeader from './SectionHeader'
 import Team from './Team'
 
+import teams from '../data/teams'
+
 const TeamShowcase = () => {
-  const teams = [
-    {
-      name: "REDPack Quantum",
-      game: "CS:GO",
-      sponsor: {
-        tag: "SPONSOR",
-        link: "http://yourbrandhere.link/"
-      },
-      players: ["AlexJ", "dyla", "7kick", "savage", "dan7"],
-    },
-    {
-      name: "REDPack Hunters",
-      game: "CS:GO",
-      sponsor: {
-        tag: "SPONSOR",
-        link: "http://yourbrandhere.link/"
-      },
-      players: ["awks", "warbringer", "exeqter", "tony77777", "erretzen"],
-    },
-    {
-      name: "REDPack UDP",
-      game: "CS:GO",
-      sponsor: {
-        tag: "SPONSOR",
-        link: "http://yourbrandhere.link/"
-      },
-      players: ["never-sleep", "velede", "Aless", "m0nkey", "mhA"],
-    },
-    {
-      name: "REDPack",
-      game: "Valorant",
-      sponsor: {
-        tag: "SPONSOR",
-        link: "http://yourbrandhere.link/"
-      },
-      players: ["17nik", "UNCAGED", "jurca", "nyw", "Javelin"],
-    },
-    {
-      name: "REDPack",
-      game: "Rocket League",
-      sponsor: {
-        tag: "SPONSOR",
-        link: "http://yourbrandhere.link/"
-      },
-      players: ["Slayer", "Deem", "Mario", "Miki"],
-    },
-    {
-      name: "REDPack",
-      game: "Fortnite",
-      sponsor: {
-        tag: "SPONSOR",
-        link: "http://yourbrandhere.link/"
-      },
-      players: ["Lynk", "28HeLL", "Mohito", "Glxck", "Ansilexx"],
-    },
-  ]
 
   return (
     <div className="p-6 bg-rose-800">
